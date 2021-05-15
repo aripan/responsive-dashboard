b0c59e594e5f0fcc237041fe9f8d3add6242c2b7
aripan <aripan017@gmail.com>
2021-05-15 22:19:22 +0200

    Top Bar

--- src/components/MainArea/TopBarSection/TopBarSection.js
+++ src/components/MainArea/TopBarSection/TopBarSection.js
@@ -1,11 +1,38 @@
 import React from "react";
 import "./TopBarSection.css";
+import { InlineIcon } from "@iconify/react";
+import mailIcon from "@iconify-icons/feather/mail";
+import messageSquare from "@iconify-icons/feather/message-square";
+import checkSquare from "@iconify-icons/feather/check-square";
+import calendarIcon from "@iconify-icons/feather/calendar";
+import shoppingCart from "@iconify-icons/feather/shopping-cart";
+import starIcon from "@iconify-icons/feather/star";
+import searchIcon from "@iconify-icons/feather/search";
+import bellIcon from "@iconify-icons/feather/bell";
 
 const TopBarSection = () => {
   return (
     <div className="top-bar-container">
       <div className="top-bar-content">
-        <h3>Contents come here</h3>
+        <div className="top-bar-content-left">
+          <InlineIcon icon={calendarIcon} className="top-bar-icon" />
+          <InlineIcon icon={messageSquare} className="top-bar-icon" />
+          <InlineIcon icon={mailIcon} className="top-bar-icon" />
+          <InlineIcon icon={checkSquare} className="top-bar-icon" />
+          <InlineIcon icon={starIcon} className="top-bar-icon" />
+        </div>
+        <div className="top-bar-content-right">
+          <div>
+            <InlineIcon icon={searchIcon} className="top-bar-icon" />
+            <InlineIcon icon={shoppingCart} className="top-bar-icon" />
+            <InlineIcon icon={bellIcon} className="top-bar-icon" />
+          </div>
+
+          <p>
+            <span style={{ fontWeight: "700" }}>Username</span> <br />
+            <span style={{ marginLeft: "25px" }}>admin</span>
+          </p>
+        </div>
       </div>
     </div>
   );
