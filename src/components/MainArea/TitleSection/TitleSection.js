85d785cdfb7ac8277342371435ec0a8876aaa3d1
aripan <aripan017@gmail.com>
2021-05-15 23:40:24 +0200

    Desktop size done

--- src/components/MainArea/TitleSection/TitleSection.js
+++ src/components/MainArea/TitleSection/TitleSection.js
@@ -12,7 +12,42 @@ const TitleSection = () => {
           sed diam voluptua.{" "}
         </p>
       </div>
-      <div className="title-items"></div>
+      <div className="title-items">
+        <div className="title-items-top">
+          <span style={{ fontWeight: "700" }}>Title</span>
+          <span style={{ fontSize: "12px" }}>updated N month(s) ago</span>
+        </div>
+        <div className="title-items-bottom">
+          <div className="title-items-bottom-one">
+            <div className="circle-one">img</div>
+            <p style={{ margin: "0.5rem" }}>
+              <span style={{ fontWeight: "700" }}>999k</span> <br />
+              <span style={{ fontSize: "12px" }}>Lorem</span>
+            </p>
+          </div>
+          <div className="title-items-bottom-two">
+            <div className="circle-two">img</div>
+            <p style={{ margin: "0.5rem" }}>
+              <span style={{ fontWeight: "700" }}>999k</span> <br />
+              <span style={{ fontSize: "12px" }}>Lorem</span>
+            </p>
+          </div>
+          <div className="title-items-bottom-three">
+            <div className="circle-three">img</div>
+            <p style={{ margin: "0.5rem" }}>
+              <span style={{ fontWeight: "700" }}>999k</span> <br />
+              <span style={{ fontSize: "12px" }}>Lorem</span>
+            </p>
+          </div>
+          <div className="title-items-bottom-four">
+            <div className="circle-four">img</div>
+            <p style={{ margin: "0.5rem" }}>
+              <span style={{ fontWeight: "700" }}>$999</span> <br />
+              <span style={{ fontSize: "12px" }}>Lorem</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -11,7 +11,7 @@ import typeIcon from "@iconify-icons/feather/type";
 import homeIcon from "@iconify-icons/feather/home";
 import creditCard from "@iconify-icons/feather/credit-card";
 import eyeIcon from "@iconify-icons/feather/eye";
-import dropdownIcon from "@iconify-icons/ls/dropdown";
+import arrowRightAlt2 from "@iconify-icons/dashicons/arrow-right-alt2";
 
 const SideBar = () => {
   const dropdownOptions = [
@@ -33,17 +33,18 @@ const SideBar = () => {
         style={{
           display: "flex",
           justifyContent: "space-around",
-          width: "180px",
+          width: "200px",
           padding: "10px 0",
           border: "1px solid black",
           borderRadius: "0.5em",
           fontSize: "16px",
+          marginLeft: "-10px",
         }}
       >
         <InlineIcon
           icon={homeIcon}
           className="dropdown-icon"
-          style={{ marginLeft: "-50px" }}
+          style={{ marginLeft: "-60px" }}
         />
         {/* <Select defaultValue={dropdownOptions[0]} options={dropdownOptions} />{" "} */}
         <select
@@ -68,36 +69,45 @@ const SideBar = () => {
       </div>
 
       <h5>CATEGORY 1</h5>
-      <ul className="side-bar-category-one">
-        <li>
-          <InlineIcon icon={mailIcon} /> Email
-        </li>
-        <li>
-          <InlineIcon icon={messageSquare} /> Chat
-        </li>
-        <li>
-          <InlineIcon icon={checkSquare} /> Todo
-        </li>
-        <li>
-          <InlineIcon icon={calendarIcon} /> Calender
-        </li>
-        <li>
-          <InlineIcon icon={shoppingCart} /> eCommerce
-        </li>
-      </ul>
+      <p>
+        <InlineIcon icon={mailIcon} className="side-bar-icon" />
+        Email
+      </p>
+      <p>
+        <InlineIcon icon={messageSquare} className="side-bar-icon" />
+        Chat
+      </p>
+      <p>
+        <InlineIcon icon={checkSquare} className="side-bar-icon" />
+        Todo
+      </p>
+      <p>
+        <InlineIcon icon={calendarIcon} className="side-bar-icon" />
+        Calender
+      </p>
+      <p>
+        <InlineIcon icon={shoppingCart} className="side-bar-icon" />
+        eCommerce{" "}
+        <InlineIcon icon={arrowRightAlt2} style={{ marginLeft: "40px" }} />
+      </p>
 
       <h5>CATEGORY 2</h5>
-      <ul className="side-bar-category-two">
-        <li>
-          <InlineIcon icon={typeIcon} /> Typography
-        </li>
-        <li>
-          <InlineIcon icon={eyeIcon} /> Feather
-        </li>
-        <li>
-          <InlineIcon icon={creditCard} /> Cards
-        </li>
-      </ul>
+      <p>
+        <InlineIcon icon={typeIcon} className="side-bar-icon" />
+        Typography
+      </p>
+      <p>
+        <InlineIcon icon={eyeIcon} className="side-bar-icon" />
+        Feather
+      </p>
+      <p>
+        <InlineIcon icon={creditCard} className="side-bar-icon" />
+        Cards
+        <button className="side-bar-new-button" style={{ marginLeft: "40px" }}>
+          new
+        </button>
+        <InlineIcon icon={arrowRightAlt2} />
+      </p>
     </div>
   );
 };
